01338094268d6552d09f640a482a9cef06a6b959
Gustavo Inacio <gsinacio94@gmail.com>
2022-04-03 23:39:29 -0300

    adds to git ignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react'
 import './styles/global.css'
 
 export function App() {
-
   return (
     <div>
-      <h1>This is Ada4 web</h1>
+      <h1>This is Ada4</h1>
     </div>
   )
 }
